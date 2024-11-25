c90da3495bfaa6d284abe0228e183f4b07370e3d
Nehir Demirli <146015700+Nehird3@users.noreply.github.com>
2024-11-25 16:50:41 +0300

    Add files via upload
    https://www.youtube.com/watch?v=DgUES4zM6As&ab_channel=NehirDemirli
    This is the video with the implementation and the description of the project.

---
+++ Term_project_EEE212-main/Objects/main.d
@@ -0,0 +1,13 @@
+./objects/main.o: main.c \
+  C:\Users\yesim\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.1\Device\Include\MKL25Z4.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Users\yesim\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm0plus.h \
+  C:\Users\yesim\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\yesim\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\yesim\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  RTE\Device\MKL25Z128xxx4\system_MKL25Z4.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\math.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\signal.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdlib.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\string.h
